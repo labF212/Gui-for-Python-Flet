e5aa539165cb24a8c0092272420dd782f254b446
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2023-03-27 16:03:50 +0100

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -3,10 +3,15 @@ import flet as ft
 def main(page: ft.Page):
     # Set page title
     page.title = "Hello, World!"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     # Create title text widget
+    
     txt = ft.Text(value="Hello, World in Flet - A GUI por Python",text_align="CENTER",weight="BOLD")
-    txt.alignment = ft.MainAxisAlignment.CENTER
+    
+    #txt.alignment = ft.MainAxisAlignment.CENTER
+    
     
     page.controls.append(txt)
     page.update()
