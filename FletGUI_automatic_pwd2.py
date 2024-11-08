12f41cbc86edbbd2e8222a3006acd084c12c2383
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2024-11-08 23:26:51 +0000

    Add files via upload

--- FletGUI_automatic_pwd2.py
+++ FletGUI_automatic_pwd2.py
@@ -17,37 +17,25 @@ def main(page: ft.Page):
         page.update()
         
     def exit_app(_):
-        page.window_destroy()
+        page.window.destroy()
 
     pwm_label1 = ft.Text("LED in pin 6")
-    pwm_label2 = ft.Text(expand=True,text_align=ft.TextAlign.END)
+    pwm_label2 = ft.Text(expand=True, text_align=ft.TextAlign.END)
 
-    row1 = ft.Row([
-        pwm_label1,
-        pwm_label2
-    ])
+    row1 = ft.Row([pwm_label1, pwm_label2])
 
     pwm_label3 = ft.Text("LED in pin 5")
-    pwm_label4 = ft.Text(expand=True,text_align=ft.TextAlign.END)
+    pwm_label4 = ft.Text(expand=True, text_align=ft.TextAlign.END)
 
     slider1 = ft.Slider(value=0, min=0, max=254, on_change=slider_changed)
 
-    row2 = ft.Row([
-        pwm_label3,
-        pwm_label4
-    ])
+    row2 = ft.Row([pwm_label3, pwm_label4])
 
     slider2 = ft.Slider(value=0, min=0, max=254, on_change=slider_changed)
     
-    button_exit= ft.ElevatedButton("Exit", icon=ft.icons.EXIT_TO_APP, on_click=exit_app)
+    button_exit = ft.ElevatedButton("Exit", icon=ft.icons.EXIT_TO_APP, on_click=exit_app)
     button_center = ft.Row([button_exit], alignment=ft.MainAxisAlignment.CENTER, spacing=10)
 
-    page.add(
-        row1,
-        slider1,
-        row2,
-        slider2,
-        button_center
-    )
+    page.add(row1, slider1, row2, slider2, button_center)
 
 ft.app(target=main)
